b9f8eb3259e5c2fee62b2b4ef15d64484afeb654
Hericlis Ventura <hericlisventura.o@gmail.com>
2022-07-29 18:40:19 -0300

    Adicionando rotas na aplicacao react atraves do router, routes e route

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
-
-import { Grid } from '@material-ui/core';
-import './App.css';
+import React from 'react';  
 import Home from './pages/Home/Home';
 import NavBar from './estatics/navbar/NavBar';
 import Footer from './estatics/footer/Footer';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
-    <>
+    
+    <Router>
       <NavBar/>
-      <Home/>
-      <Footer/>
-    </>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/" element={<Home/>}/>
+      
+
+      </Routes>
+        <Footer/>
+    </Router>
+  
   );
 }
 
